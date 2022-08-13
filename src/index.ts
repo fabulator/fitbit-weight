07e1cbbca7e27c5585a01a9ee2be436d002a33c9
Michal Ozogán <michal@ozogan.eu>
2022-08-13 15:04:00 +0000

    add mq

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import 'cross-fetch/polyfill';
 import { fastify } from 'fastify';
+import { Queue } from 'bullmq';
 import { Api, ApiScope, SubscriptionCollection } from 'fitbit-api-handler';
 import rawBody from 'fastify-raw-body';
 import { config } from 'dotenv';
 
 config();
 
-const { API_KEY, API_SECRET, BASE_URI, AUTHORIZE_PATH, LOGIN_PATH, LISTEN_PATH, VERIFY_TOKEN, PORT } = process.env;
+const { API_KEY, API_SECRET, BASE_URI, AUTHORIZE_PATH, LOGIN_PATH, LISTEN_PATH, VERIFY_TOKEN, PORT, QUEUE_NAME, REDIS_HOST } = process.env;
 
 const api = new Api(API_KEY, API_SECRET);
 
 const app = fastify({ logger: true });
 
 const REDIRECT_URL = `${BASE_URI}${AUTHORIZE_PATH}`;
+
+type FitbitData = {
+    collectionType: 'body';
+    date: string;
+    ownerId: string;
+    ownerType: string;
+    subscriptionId: string;
+};
+
+const queue = new Queue<FitbitData>(QUEUE_NAME, { connection: { host: REDIS_HOST } });
 
 (async () => {
     await app.register(rawBody, {
@@ -44,7 +55,7 @@ const REDIRECT_URL = `${BASE_URI}${AUTHORIZE_PATH}`;
         },
     });
 
-    app.post(LISTEN_PATH, {
+    app.post<{ Body: FitbitData[] }>(LISTEN_PATH, {
         handler: async (request) => {
             if (typeof request.rawBody !== 'string') {
                 throw new Error('Invalid request. It must be the string.');
@@ -57,14 +68,15 @@ const REDIRECT_URL = `${BASE_URI}${AUTHORIZE_PATH}`;
             }
 
             if (!api.verifyFitbitRequest(request.rawBody, fitbitSignature)) {
-                console.error('Invalid signature', request.rawBody, request.headers);
+                throw new Error('Invalid signature.');
             }
 
-            const body = JSON.parse(request.rawBody);
-
-            body.forEach((item: Record<string, unknown>) => {
-                console.log(item);
-            });
+            // eslint-disable-next-line compat/compat
+            await Promise.all(
+                request.body.map(async (item) => {
+                    await queue.add(`Weight from ${item.date}`, item);
+                }),
+            );
             return '';
         },
     });
