14e2e4e3f84e930475e856b14cd92b98567e6a25
Michal Ozogán <michal@ozogan.eu>
2022-08-14 05:18:57 +0000

    fix date resolving

--- src/worker-weight.ts
+++ src/worker-weight.ts
@@ -1,19 +1,20 @@
 import { Worker } from 'bullmq';
 import { ApiWeight } from 'fitbit-api-handler/src/types/api/ApiWeight';
+import { DateTime } from 'luxon';
 import { logger, queueSettings, stravaApi, tokenService } from './common';
 
 const { QUEUE_WEIGHT_NAME } = process.env;
 
 logger.info('Booting up...');
 
-const worker = new Worker<ApiWeight>(
+const worker = new Worker<Omit<ApiWeight, 'datetime'> & { datetime: string }>(
     QUEUE_WEIGHT_NAME,
     async (job) => {
         logger.info(job.data, 'Processing...');
 
         // strava can have only one weight, no history
         // check that item is not too old
-        if (job.data.datetime.diffNow().as('day') <= 1) {
+        if (DateTime.fromISO(job.data.datetime).diffNow().as('day') <= 1) {
             const token = await tokenService.get('strava');
 
             if (!token) {
